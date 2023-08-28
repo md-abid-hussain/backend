04b13d6e305094c76b65d0f735556cc3653fc387
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-08-28 10:32:45 +0530

    created login request limiter

---
+++ middleware/loginLimiter.js
@@ -0,0 +1,18 @@
+const rateLimit = require('express-rate-limit')
+const {logEvent} = require('./logEvent')
+
+const loginLimiter = rateLimit({
+    windowMs: 60*1000,
+    max:5,  // Limit each IP to 5 login per 'window' per minute
+    message:{
+        message:'Too many login attempts from this IP, please try after a 60 second pause'
+    },
+    handler:(req,res,next,options)=>{
+        logEvent(`Too many requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`,'errlog.log')
+        res.status(options.statusCode).send(options.message)
+    },
+    standardHeaders: true, // Return rate limit info in the 'RateLimit-*' headers
+    legacyHeaders:false // Disable the 'x-RateLimit-*' headers
+})
+
+module.exports = loginLimiter
