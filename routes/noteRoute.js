29d63120b6f27bcbe2206244e90c98b7372c2ac6
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-08-28 10:34:09 +0530

    added verify jwt middleware to note route

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const notesController = require('../controller/notesController')
 const addTicketNum = require('../middleware/addTicketNum')
+const verifyJWT = require('../middleware/verifyJWT')
+
+router.use(verifyJWT)
 
 router.route('/')
     .get(notesController.getAllNotes)
