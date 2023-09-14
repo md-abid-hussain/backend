7f2a7e1e23d516685af22ff3f510979a09fc6a63
MD ABID HUSSAIN <abid0hussain1733@gmail.com>
2023-09-14 23:57:14 +0530

    fixed logerror

--- middleware/logError.js
+++ middleware/logError.js
@@ -2,8 +2,13 @@ const { logEvents } = require('./logEvent');
 
 const errorHandler = (err, req, res, next) => {
     logEvents(`${err.name}\t${err.message}`, 'errorLog.txt');
-    console.error(err);
-    res.status(500).send(err.message)
+    console.log(err.stack);
+
+    const status = res.statusCode ? res.statusCode : 500
+
+    res.status(status)
+
+    res.json({message:err.message, isError:true})
 }
 
 module.exports = errorHandler;
